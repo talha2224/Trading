8732ffb5d86e9436423a08985151ea6af7eacdf1
Syed Talha Haider <talha.haider@eusopht.com>
2024-09-19 00:19:23 +0500

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/login' element={<LoginPage/>}/>
+      <Route path='/' element={<LoginPage/>}/>
       <Route path='/register' element={<RegisterPage/>}/>
       <Route path='/admin/home' element={<HomePage/>}/>
     </Routes>
