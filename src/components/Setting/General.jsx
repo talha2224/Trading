8d6b52672d32497d22cb13e1e18c323a5716b231
Minhal Rizvi <rizvi4600779@cloud.neduet.edu.pk>
2025-05-11 21:37:42 +0500

    Deployment issued fixed

--- src/components/Setting/General.jsx
+++ src/components/Setting/General.jsx
@@ -18,7 +18,7 @@ const General = () => {
         const fetchConfig = async () => {
           try {
             setLoading(true);
-            const response = await axios.get('http://localhost:5000/api/platformconfig/platformconfig', {
+            const response = await axios.get('https://backend-nine-tau-59.vercel.app/api/platformconfig/platformconfig', {
               headers: {
                 Authorization: `Bearer ${localStorage.getItem('token')}`
               }
